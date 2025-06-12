f78c3271a562dd0de0a61b05d600ca66bb181118
gs <gs@xyz.de>
2025-06-12 22:01:20 +0200

    added version info in SettingsPage

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -12,7 +12,9 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  final textStyle = const TextStyle(fontSize: 18);
+  final textStyleNormal = const TextStyle(fontSize: 18);
+  final textStyleUnderlined =
+      const TextStyle(fontSize: 18, decoration: TextDecoration.underline);
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +34,13 @@ class _SettingsPageState extends State<SettingsPage> {
             Padding(
               padding: const EdgeInsets.fromLTRB(10.0, 20, 10, 10),
               child: Text("Which controls should be shown to move the player?",
-                  style: textStyle),
+                  style: textStyleNormal),
             ),
             // without a SizedBox around the RadioListTile I got
             // "BoxConstraints forces an infinite width."
             RadioListTile<bool>(
-              title:
-                  Text("buttons with left and right arrows", style: textStyle),
+              title: Text("buttons with left and right arrows",
+                  style: textStyleNormal),
               value: true,
               groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
               onChanged: (bool? value) {
@@ -53,7 +55,7 @@ class _SettingsPageState extends State<SettingsPage> {
             RadioListTile<bool>(
               title: Text(
                   "an extra panning area to move the player with gestures",
-                  style: textStyle),
+                  style: textStyleNormal),
               value: false,
               groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
               onChanged: (bool? value) {
@@ -67,12 +69,32 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
             Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Text(
-                  "Remark:\nIn case you have a keyboard, you can use ArrowLeft and ArrowRight keys to move the player "
-                  "and ArrowUp or Space key to fire a missile.\n"
-                  "Panning in the playing area moves the player too, but in this case your finger might hide the player or the balls, "
-                  "therefore the option for an extra panning area below the playground.",
-                  style: textStyle),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Remark", style: textStyleUnderlined),
+                  Text(
+                      "In case you have a keyboard, you can use ArrowLeft and ArrowRight keys to move the player "
+                      "and ArrowUp or Space key to fire a missile.\n"
+                      "Panning in the playing area moves the player too, but in this case your finger might hide the player or the balls, "
+                      "therefore the option for an extra panning area below the playground.",
+                      style: textStyleNormal),
+                ],
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Version info", style: textStyleUnderlined),
+                  Text(
+                      "This is version 0.2 from 12-June-2025,\n"
+                      "developped during a Flutter training at FDG.",
+                      style: textStyleNormal),
+                ],
+              ),
             ),
           ],
         ),
