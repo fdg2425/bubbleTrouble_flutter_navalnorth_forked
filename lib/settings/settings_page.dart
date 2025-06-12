4be741bb3aef664451d947b7b8ddd82711a07b28
gs <gs@xyz.de>
2025-06-12 21:23:14 +0200

    SingleChildScrollView in the SettingsPage added

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -24,54 +24,58 @@ class _SettingsPageState extends State<SettingsPage> {
           child: Divider(height: 1, thickness: 1, color: Colors.grey),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10.0, 20, 10, 10),
-            child: Text("Which controls should be shown to move the player?",
-                style: textStyle),
-          ),
-          // without a SizedBox around the RadioListTile I got
-          // "BoxConstraints forces an infinite width."
-          RadioListTile<bool>(
-            title: Text("buttons with left and right arrows", style: textStyle),
-            value: true,
-            groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
-            onChanged: (bool? value) {
-              if (value != null) {
-                // next setState is needed to update the Radiobuttons
-                setState(() {
-                  widget.settingsProvider.showButtonsForPlayerMovement = true;
-                });
-              }
-            },
-          ),
-          RadioListTile<bool>(
-            title: Text(
-                "an extra panning area to move the player with gestures",
-                style: textStyle),
-            value: false,
-            groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
-            onChanged: (bool? value) {
-              if (value != null) {
-                setState(() {
-                  widget.settingsProvider.showButtonsForPlayerMovement = false;
-                });
-              }
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-                "Remark:\nIn case you have a keyboard, you can use ArrowLeft and ArrowRight keys to move the player "
-                "and ArrowUp or Space key to fire a missile.\n"
-                "Panning in the playing area moves the player too, but in this case your finger might hide the player or the balls, "
-                "therefore the option for an extra panning area below the playground.",
-                style: textStyle),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 20, 10, 10),
+              child: Text("Which controls should be shown to move the player?",
+                  style: textStyle),
+            ),
+            // without a SizedBox around the RadioListTile I got
+            // "BoxConstraints forces an infinite width."
+            RadioListTile<bool>(
+              title:
+                  Text("buttons with left and right arrows", style: textStyle),
+              value: true,
+              groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
+              onChanged: (bool? value) {
+                if (value != null) {
+                  // next setState is needed to update the Radiobuttons
+                  setState(() {
+                    widget.settingsProvider.showButtonsForPlayerMovement = true;
+                  });
+                }
+              },
+            ),
+            RadioListTile<bool>(
+              title: Text(
+                  "an extra panning area to move the player with gestures",
+                  style: textStyle),
+              value: false,
+              groupValue: widget.settingsProvider.showButtonsForPlayerMovement,
+              onChanged: (bool? value) {
+                if (value != null) {
+                  setState(() {
+                    widget.settingsProvider.showButtonsForPlayerMovement =
+                        false;
+                  });
+                }
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                  "Remark:\nIn case you have a keyboard, you can use ArrowLeft and ArrowRight keys to move the player "
+                  "and ArrowUp or Space key to fire a missile.\n"
+                  "Panning in the playing area moves the player too, but in this case your finger might hide the player or the balls, "
+                  "therefore the option for an extra panning area below the playground.",
+                  style: textStyle),
+            ),
+          ],
+        ),
       ),
     );
   }
